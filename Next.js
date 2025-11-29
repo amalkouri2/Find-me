bedb6e70fc8679bb6a04c28e6ce833b2c42a675f
amalkouri2 <amalkouri20@gmail.com>
2025-11-30 00:32:47 +0300

    Initialize Next.js project structure

---
+++ Next.js
@@ -0,0 +1,11 @@
+Find/
+ ├── app/
+ │    ├── globals.css
+ │    ├── layout.js
+ │    └── page.js
+ ├── public/
+ │    └── logo.png
+ ├── package.json
+ ├── next.config.js
+ ├── jsconfig.json
+ └── README.md
